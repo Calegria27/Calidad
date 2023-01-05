ef45f519c7911053e881c88c647f68f83cad6c33
Calegria27 <carlos.alegria@independencia.cl>
2023-01-05 16:17:30 -0300

    dropdown added

--- src/Views/pages/MainView.jsx
+++ src/Views/pages/MainView.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Authcontext } from '../../Auth/context/Authcontext';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 
@@ -7,17 +9,27 @@ import { Authcontext } from '../../Auth/context/Authcontext';
 
 
 const MainView = () => {
-  
-  const {user}=useContext(Authcontext);
-      
+
+  const { user } = useContext(Authcontext);
+
   return (
     <div>
       <div className='container' style={{ display: 'flex', justifyContent: 'left' }} >
         <h2>Usuario: {user}</h2>
       </div>
-          
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
 
-     
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+
 
     </div>
 
