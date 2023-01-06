f3b96895c57d01a0e2cd6aee0e1c9c7a12fc91ab
Calegria27 <carlos.alegria@independencia.cl>
2023-01-06 16:06:51 -0300

    avance hasta dropdown de sectores

--- src/Views/pages/MainView.jsx
+++ src/Views/pages/MainView.jsx
@@ -15,12 +15,19 @@ const MainView = () => {
 
   const { user } = useContext(Authcontext);
   const empresasurl = "http://192.168.1.17/user/empresas"
-  const obrasurl="http://192.168.1.17/user/empresas/obras"
+  const obrasurl = "http://192.168.1.17/user/empresas/obras"
+  const sectorurl = "http://192.168.1.17/user/empresas/obras/sector"
+  const Ufisicaurl = "http://192.168.1.17/user/empresas/obras/sector/ufisica"
   const [dataempresas, setDataempresas] = useState(null);
   const [dataobras, setDataobras] = useState(null);
+  const [datasectores, setDataSectores] = useState(null);
   const [selectedOption, setSelectedOption] = useState("Seleccione empresa");
+  const [selectedOptionO, setSelectedOptionO] = useState("");
   const [selectedOptionName, setSelectedOptionName] = useState("Seleccione empresa");
   const [selectedOptionNameObra, setSelectedOptionNameObra] = useState("Seleccione obra");
+  const [isVisible, setIsVisible] = useState(false);
+  const [selectedSector, setSelectedSector] = useState("")
+  const [selecteduFisica,setSelectedUFisica]=useState("")
 
 
   useEffect(() => {
@@ -31,8 +38,7 @@ const MainView = () => {
   }, []);
 
   useEffect(() => {
-    const data={Usu_Cuenta:user,CtoEmpresa:selectedOption}
-    console.log(data)
+    const data = { Usu_Cuenta: user, CtoEmpresa: selectedOption }
     axios.post(obrasurl, data)
       .then((response) => {
         setDataobras(response.data)
@@ -40,50 +46,83 @@ const MainView = () => {
   }, [selectedOption]);
 
 
+  useEffect(() => {
+    const data = { CtoEmpresa: selectedOption, CtoCodigo: selectedOptionO }
+    axios.post(sectorurl, data)
+      .then((response) => {
+        setDataSectores(response.data)
+      })
+  }, [selectedOptionO])
+
+  useEffect(()=>{
+    const data ={CtoEmpresa: selectedOption, CtoCodigo: selectedOptionO, Sector:selectedSector}
+    axios.post(Ufisicaurl,data)
+    .then((response)=>{
+      console.log(response)
+    }
+   )
+  }, [selectedSector])
 
 
-  const handleOptionClick = (option,name) => {
-    console.log(option)
+  const handleOptionClick = (option, name) => {
     setSelectedOption(option)
     setSelectedOptionName(name)
+    if (isVisible) {
+      setIsVisible(!isVisible)
+    }
   }
 
 
-  const handleOptionClickObra= (option,name) => {
+  const handleOptionClickObra = (option, name) => {
     console.log(option)
-    setSelectedOption(option)
+    setSelectedOptionO(option)
     setSelectedOptionNameObra(name)
+    if (!isVisible) {
+      setIsVisible(!isVisible)
+    }
+
   }
 
+  const handleOptionClickSector = (option) => {
+    setSelectedSector(option)
+  }
 
   const empresas = dataempresas
     ? Object.entries(dataempresas).map(([key, value]) => (
 
-      <Dropdown.Item key={key}  onClick={() => handleOptionClick(dataempresas[key]["empCodigo"],dataempresas[key]["empNombre"])}
-      >{`${dataempresas[key]["empCodigo"]}: ${dataempresas[key]["empNombre"]}}`}</Dropdown.Item>
+      <Dropdown.Item key={key} onClick={() => handleOptionClick(dataempresas[key]["empCodigo"], dataempresas[key]["empNombre"])}
+      >{`${dataempresas[key]["empCodigo"]}: ${dataempresas[key]["empNombre"]}`}</Dropdown.Item>
     ))
 
     : null;
 
-    const obras = dataobras
+  const obras = dataobras
     ? Object.entries(dataobras).map(([key, value]) => (
 
-      <Dropdown.Item key={key} onClick={() => handleOptionClickObra(dataobras[key]["CtoCodigo"],dataobras[key]["Obras"])}
-      >{`${dataobras[key]["CtoCodigo"]}: ${dataobras[key]["Obras"]}}`}</Dropdown.Item>
+      <Dropdown.Item key={key} onClick={() => handleOptionClickObra(dataobras[key]["CtoCodigo"], dataobras[key]["Obras"])}
+      >{`${dataobras[key]["CtoCodigo"]}: ${dataobras[key]["Obras"]}`}</Dropdown.Item>
     ))
 
     : null;
 
+  const sectores = datasectores
+    ? Object.entries(datasectores).map(([key, value]) => (
+
+      <Dropdown.Item key={key} onClick={() => handleOptionClickSector(datasectores[key]["Sector"])}
+      >{`${datasectores[key]["Sector"]}`}</Dropdown.Item>
+    ))
+
+    : null;
 
   return (
     <div>
       <div className="container" style={{ justifyContent: "left" }}>
         <h2>Usuario: {user}</h2>
       </div>
-      <div className='container-dropdown'style={{display: "inline-block"}}>
+      <div className='container-dropdown' style={{ display: "inline-block" }}>
         <Dropdown>
           <Dropdown.Toggle title={selectedOption} variant="success" id="dropdown-basic">
-             {selectedOptionName}
+            {selectedOptionName}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
@@ -96,10 +135,39 @@ const MainView = () => {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-              {obras}
+            {obras}
           </Dropdown.Menu>
         </Dropdown>
       </div>
+
+      {isVisible &&
+        <div className='containerapear'>
+          <Dropdown>
+            <Dropdown.Toggle>
+              {selectedSector}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {sectores}
+            </Dropdown.Menu>
+          </Dropdown>
+          { selectedSector!==""&&
+            <div className='containerapear'>
+              <Dropdown>
+                <Dropdown.Toggle>
+                  {selectedSector}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {sectores}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+
+          }
+        </div>
+
+      }
+
+
 
 
     </div>
